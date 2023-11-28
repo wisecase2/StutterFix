5110d567ba235fdb16bc2d7129338c518a7bbbef
wisecase2 <43482628+wisecase2@users.noreply.github.com>
2023-11-28 20:36:34 -0300

    small changes

--- src/main/java/net/wisecase2/stutterfix/mixin/MinecraftClientMixin.java
+++ src/main/java/net/wisecase2/stutterfix/mixin/MinecraftClientMixin.java
@@ -9,13 +9,7 @@ public class MinecraftClientMixin {
 
     @ModifyArg(method = "run()V", at = @At(value = "INVOKE", target = "java/lang/Thread.setPriority (I)V"), index = 0)
     private int setMinecraftClientPriority(int p) {
-        int available_processors = Runtime.getRuntime().availableProcessors();
-        return switch (available_processors) {
-            case 1, 2, 3, 4 -> 5;
-            case 5, 6 -> 6;
-            case 7, 8 -> 7;
-            default -> 8;
-        };
+        return 10;
     }
 
 }
