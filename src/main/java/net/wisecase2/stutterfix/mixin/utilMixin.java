a919fbcc63657a37424626d444f57cd042a4bd13
wisecase2 <43482628+wisecase2@users.noreply.github.com>
2023-12-06 16:53:54 -0300

    Change to mc 1.20.3

--- src/main/java/net/wisecase2/stutterfix/mixin/utilMixin.java
+++ src/main/java/net/wisecase2/stutterfix/mixin/utilMixin.java
@@ -5,6 +5,8 @@ import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.*;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -13,19 +15,20 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Util.class)
 public abstract class utilMixin {
+
 	@ModifyVariable(method = "createWorker", at = @At("STORE"), ordinal = 0)
 	private static int setNumThreadsOfMainWorker(int p) {
-		if(p >= 7){
+		if (p >= 7) {
 			return p - 4;
-		}else if(p >= 3) {
+		} else if (p >= 3) {
 			return 1;
-		}else{
+		} else {
 			return 1;
 		}
 	}
 
 	@Inject(method = "method_28123", at = @At(value = "INVOKE", target = "java/util/concurrent/ForkJoinWorkerThread.setName (Ljava/lang/String;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-	private static void setPriorityOfThreadMainWorker(String string, ForkJoinPool forkjoinpool, CallbackInfoReturnable ci, ForkJoinWorkerThread forkJoinWorkerThread) {
+	private static void setPriorityOfThreadMainWorker(String string, AtomicInteger val, ForkJoinPool forkjoinpool, CallbackInfoReturnable ci, ForkJoinWorkerThread forkJoinWorkerThread) {
 		forkJoinWorkerThread.setPriority(1);
 	}
 }
