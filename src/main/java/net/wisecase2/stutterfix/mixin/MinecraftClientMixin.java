fdeb2d3a1ea5ec38ca18b0250a30ef65f8fb1692
wisecase2 <43482628+wisecase2@users.noreply.github.com>
2023-12-07 18:08:11 -0300

    Fix yield(), remove unnecessary code
    ​
    367 / 5.000
    Resultados de tradução
    Resultado da tradução
    A modification was made to remove yield, it will only be removed if you are using sodium, because it has a "wait for gpu" system, so it is not necessary to use yield() on the client thread in the middle of rendering. Yield() was placed in vanilla probably to prevent the client thread from going too far ahead of the GPU, this problem was solved by the sodium mod.

--- src/main/java/net/wisecase2/stutterfix/mixin/MinecraftClientMixin.java
+++ src/main/java/net/wisecase2/stutterfix/mixin/MinecraftClientMixin.java
@@ -1,23 +1,18 @@
 package net.wisecase2.stutterfix.mixin;
 
-import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.util.Util;
-import net.minecraft.util.profiler.Profiler;
-import net.minecraft.util.thread.ReentrantThreadExecutor;
-import org.objectweb.asm.Opcodes;
+import net.fabricmc.loader.api.FabricLoader;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(net.minecraft.client.MinecraftClient.class)
 public class MinecraftClientMixin {
     @Redirect(method = "render(Z)V", at = @At(value = "INVOKE", target = "java/lang/Thread.yield ()V"))
     private void removeThreadYield() {
+        //Remove yield() only for sodium as it has the "Wait for GPU" system
+        if(!FabricLoader.getInstance().isModLoaded("sodium")){
+            Thread.yield();
+        }
     }
 
 }
