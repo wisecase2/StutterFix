d21ab0dbd56829119b0882f1f2bfa62585e6776a
wisecase2 <43482628+wisecase2@users.noreply.github.com>
2023-12-09 19:53:20 -0300

    update to 0.1.6
    - Chunk generation is faster

--- src/main/java/net/wisecase2/stutterfix/mixin/utilMixin.java
+++ src/main/java/net/wisecase2/stutterfix/mixin/utilMixin.java
@@ -4,14 +4,8 @@ import net.minecraft.util.Util;
 
 import org.spongepowered.asm.mixin.*;
 
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Util.class)
 public abstract class utilMixin {
@@ -20,15 +14,8 @@ public abstract class utilMixin {
 	private static int setNumThreadsOfMainWorker(int p) {
 		if (p >= 7) {
 			return p - 4;
-		} else if (p >= 3) {
-			return 1;
 		} else {
 			return 1;
 		}
-	}
-
-	@Inject(method = "method_28123", at = @At(value = "INVOKE", target = "java/util/concurrent/ForkJoinWorkerThread.setName (Ljava/lang/String;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-	private static void setPriorityOfThreadMainWorker(String string, AtomicInteger val, ForkJoinPool forkjoinpool, CallbackInfoReturnable ci, ForkJoinWorkerThread forkJoinWorkerThread) {
-		forkJoinWorkerThread.setPriority(1);
 	}
 }
